defdb48ecb46b64c50bcfd466c36c44f78e2cbc4
Kevin Seitz <1263088+kseitz10@users.noreply.github.com>
2021-09-24 16:07:14 -0500

    Scaffolded app management.
    Edit does not work.

---
+++ AuthorizationServer/Pages/Applications/Delete.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AuthorizationServer.Data;
+
+using OpenIddict.Abstractions;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace AuthorizationServer.Pages.Applications
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly OpenIddictApplicationManager<OpenIddictEntityFrameworkCoreApplication> _appManager;
+
+        public DeleteModel(OpenIddictApplicationManager<OpenIddictEntityFrameworkCoreApplication> appManager)
+        {
+            _appManager = appManager;
+        }
+
+        [BindProperty]
+        public OpenIddictApplicationDescriptor OpenIddictApplication { get; set; }
+
+        [BindProperty]
+        public string Id { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var efObject = await _appManager.FindByIdAsync(id);
+            if (efObject == null)
+            {
+                return NotFound();
+            }
+
+            OpenIddictApplication = new OpenIddictApplicationDescriptor();
+            Id = efObject.Id;
+            await _appManager.PopulateAsync(OpenIddictApplication, efObject);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null || OpenIddictApplication == null)
+            {
+                return NotFound();
+            }
+
+            var existing = await _appManager.FindByIdAsync(id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            await _appManager.DeleteAsync(existing);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ AuthorizationServer/Pages/Applications/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using OpenIddict.Abstractions;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace AuthorizationServer.Pages.Applications
+{
+    public class EditModel : PageModel
+    {
+        private readonly OpenIddictApplicationManager<OpenIddictEntityFrameworkCoreApplication> _appManager;
+
+        public EditModel(OpenIddictApplicationManager<OpenIddictEntityFrameworkCoreApplication> appManager)
+        {
+            _appManager = appManager;
+        }
+
+        [BindProperty]
+        public OpenIddictApplicationDescriptor OpenIddictApplication { get; set; }
+        
+        [BindProperty]
+        public string Id { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var efObject = await _appManager.FindByIdAsync(id);
+            if (efObject == null)
+            {
+                return NotFound();
+            }
+
+            OpenIddictApplication = new OpenIddictApplicationDescriptor();
+            Id = efObject.Id;
+            await _appManager.PopulateAsync(OpenIddictApplication, efObject);
+
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (OpenIddictApplication == null || string.IsNullOrEmpty(Id))
+            {
+                return NotFound();
+            }
+
+            var existing = await _appManager.FindByIdAsync(Id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            await _appManager.UpdateAsync(existing);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
